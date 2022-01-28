afc648aaccd84afeba77f9db85034f11c9af3033
soshetty1 <soumyamshetty133@gmail.com>
2022-01-28 11:11:03 -0600

    third commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/shared/header/header.component';
 import { FooterComponent } from './component/shared/footer/footer.component';
 import { NavComponent } from './component/shared/nav/nav.component';
+import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
