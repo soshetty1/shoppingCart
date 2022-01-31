54c757ca5bb3b0eead89b31a80a650088a3c1a23
soshetty1 <soumyamshetty133@gmail.com>
2022-01-31 10:54:30 -0600

    created new components in  shopping cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './component/shared/header/header.component';
 import { FooterComponent } from './component/shared/footer/footer.component';
 import { NavComponent } from './component/shared/nav/nav.component';
 import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FiltersComponent } from './component/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './component/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './component/shopping-cart/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.c
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
