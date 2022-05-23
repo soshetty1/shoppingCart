1af4f7080bfdab7e84183b02ff35239fb97e7a07
soshetty1 <soumyamshetty133@gmail.com>
2022-05-23 15:09:10 -0500

    removed  hardcoded values for cartitems

--- src/app/component/shopping-cart/cart/cart.component.ts
+++ src/app/component/shopping-cart/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { MessengerService } from 'src/app/services/messenger.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -6,21 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-cartItems =[
-  {id: 1,productid:1,productName: 'test1', quantity:4, price:100 },
-{id: 1,productid:3,productName: 'test2' ,quantity:4, price:400 },
-{id: 1,productid:6, productName: 'test3', quantity:2, price:200 },
-{id: 1,productid:1,productName: 'test4' ,quantity:1, price:150 }
-];
+// cartItems =[
+// {id: 1,productid:1,productName: 'test1', quantity:4, price:100 },
+// {id: 1,productid:3,productName: 'test2' ,quantity:4, price:400 },
+// {id: 1,productid:6, productName: 'test3', quantity:2, price:200 },
+// {id: 1,productid:1,productName: 'test4' ,quantity:1, price:150 }
+// ];
+cartItems : {productName:string;quantity:number, price:number}[]=[];
 
 cartTotal=0
 
-  constructor() { }
+ constructor(private msg:MessengerService) { }
 
   ngOnInit() {
-    this.cartItems.forEach(item => {
-      this.cartTotal += (item.quantity * item.price)
-    })
+    this.msg.getMsg().subscribe((product: any) => { 
+      this.cartItems.push({
+         productName: product.name, 
+         quantity: 1,
+         price: product.price
+        })
+        this.cartTotal = 0; 
+        this.cartItems.forEach(item => { 
+          this.cartTotal += (item['quantity'] * item['price']) 
+        }) 
+      })
   }
 
 }
+
+
--- src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
+++ src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit ,Input } from '@angular/core';
 import { Product } from 'src/app/model/product';
-
+import { MessengerService } from 'src/app/services/messenger.service';
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
@@ -10,11 +10,14 @@ export class ProductItemComponent implements OnInit {
 
  @Input() productItem!: Product;
 
-  constructor() { 
+  constructor(private Msg: MessengerService) { 
     
   }
 
   ngOnInit() {
+  }
+  handleAddToCart(){
+    this.Msg.sendMsg(this.productItem)
   }
 
 }
